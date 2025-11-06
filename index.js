0714dc7094f95265ef4db30b75afba9942ae0c15
Thane <thabay0606@students.maquoketaschools.org>
2025-11-06 20:27:47 +0000

    This might work but I doubt it.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+var calculateTotal = (subtotal, shipping = 2.50) => {
+    return subtotal + shipping;
+};
 
+var printTopThreeHeadlines = (...value) => {
+    return value[0], value[1], value[2];
+};
+
+printTopThreeHeadlines = printTopThreeHeadlines("headlines", "headlines", "headlines");
